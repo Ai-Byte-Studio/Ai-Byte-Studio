7aa47b37350672ee5c7687b6a87f753d926389b7
Mirza_Muhammad_Abdullah_Baig <aibytestudio@gmail.com>
2024-08-25 16:20:55 +0500

    contact us page addition

--- Ai/pagemgr/urls.py
+++ Ai/pagemgr/urls.py
@@ -6,6 +6,11 @@ from . import views # type: ignore
 # from Ai.pagemgr import views
 # from . import views bcz it import all urls.py form all apps imagine same name of function in views so it voilate commands
 urlpatterns = [
-    path('',views.index , name='index')
+    path('',views.index , name='index'),
+    path('contactus',views.contactus , name='contactus'),
+    path('index',views.index , name='index'),
+    path('Services',views.Services , name='Services'),
+    path('About',views.About , name='About')
+
     
 ]
--- Ai/pagemgr/views.py
+++ Ai/pagemgr/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render # type: ignore
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+def contactus(request):
+    return render(request,'contactus.html')
+def Services(request):
+    return render(request,'Services.html')
+def About(request):
+    return render(request,'About.html')
